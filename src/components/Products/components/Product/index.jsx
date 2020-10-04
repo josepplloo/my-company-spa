c008dd1db41fbac68c54ae656eeda570bf52642a
Jose Miguel <josepplloo@gmail.com>
2020-10-03 22:41:58 -0500

    add products contiainer layout (#2)

--- src/components/Products/components/Product/index.jsx
+++ src/components/Products/components/Product/index.jsx
@@ -7,27 +7,43 @@ import {
   CardSubtitle,
   CardHeader,
 } from "@react-md/card";
-import { Text } from 'react-md';
+import { Text, TextContainer, Grid } from 'react-md';
+import styles from './styles.module.scss';
 
 function Product() {
   const { productId } = useParams();
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{productId}</CardTitle>
-        <CardSubtitle>Subtitle</CardSubtitle>
-      </CardHeader>
-      <CardContent>
-        <img src="https://picsum.photos/600/337?image=402" alt="" />
-        <Text>
-          Duis lacinia lectus sed enim placerat, non consequat arcu tincidunt.
-          Pellentesque vel.
-        </Text>
-        <span><Text className="subtitle-2">Stock</Text><Text>1231</Text></span>
-        <span><Text className="subtitle-2">Price</Text><Text>45454</Text></span>
-      </CardContent>
-    </Card>
+    <>
+      <TextContainer>
+        <Text>Showing 10 products - Hidden: 5</Text>
+      </TextContainer>
+      <Card>
+        <CardHeader>
+          <CardTitle>{productId}</CardTitle>
+          <CardSubtitle>Subtitle</CardSubtitle>
+        </CardHeader>
+        <CardContent>
+          <Grid columns={2}>
+            <img className={styles.image} src="https://picsum.photos/600/337?image=402" alt="product" />
+            <TextContainer>
+              <Text type="body-2">
+                Duis lacinia lectus sed enim placerat, non consequat arcu tincidunt.
+                Pellentesque vel.
+              </Text>
+              <span>
+                <Text type="subtitle-2">Stock:</Text>
+                <Text>1231</Text>
+              </span>
+              <span>
+                <Text type="subtitle-2">Price:</Text>
+                <Text>45454</Text>
+              </span>
+            </TextContainer>
+          </Grid>
+        </CardContent>
+      </Card>
+    </>
   );
 }
 
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, List, ListItem, Divider, } from 'react-md';
+import { Text, List, ListItem, Divider, Grid, GridCell } from 'react-md';
 import {
   Switch,
   Route,
@@ -13,27 +13,31 @@ export default function Products() {
   let { path, url } = useRouteMatch();
 
   return (
-    <>
-      <List>
-        <Text type="subtitle-1">Products</Text>
-        {
-          LINKS.map(({ name, path, icon }, index) =>
-            <div key={name}>
-              <Link to={`${url}${path}`}>
-                <ListItem id={`L-${path}`} leftAddon={icon}>{name}</ListItem>
-              </Link>
-              {index === 0 && <Divider />}
-            </div>)
-        }
-      </List>
-      <Switch>
-        <Route exact path={path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-        <Route path={`${path}/:productId`}>
-          <Product />
-        </Route>
-      </Switch>
-    </>
+    <Grid columns={3} tabletColumns={1} phoneColumns={1}>
+      <GridCell >
+        <List>
+          <Text type="subtitle-1">Products</Text>
+          {
+            LINKS.map(({ name, path, icon }, index) =>
+              <div key={name}>
+                <Link to={`${url}${path}`}>
+                  <ListItem id={`L-${path}`} leftAddon={icon}>{name}</ListItem>
+                </Link>
+                {index === 0 && <Divider />}
+              </div>)
+          }
+        </List>
+      </GridCell>
+      <GridCell>
+        <Switch>
+          <Route exact path={path}>
+            <Product />
+          </Route>
+          <Route path={`${path}/:productId`}>
+            <Product />
+          </Route>
+        </Switch>
+      </GridCell>
+    </Grid>
   );
 }
